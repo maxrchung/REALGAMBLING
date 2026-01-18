36e8a342c914a39270094b7f90e54255f8441bd6
snady <sndyfang@gmail.com>
2026-01-18 14:34:04 -0800

    prefab

--- real-gambling/Assets/_Assets/Scripts/Gameplay/GamblingWheel.cs
+++ real-gambling/Assets/_Assets/Scripts/Gameplay/GamblingWheel.cs
@@ -1,13 +1,16 @@
 using System.Linq;
 using GambleCore.Gambling;
 using GambleCore.Interface;
+using GambleCore;
 using GambleCore.Util;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GamblingWheelController : AbstractWheel
 {
+    public override int Height => 3;
     private readonly ISymbol[] _symbols;
-    protected override ISymbol[] Symbols { get; }
+    protected override ISymbol[] Symbols => _symbols.ToArray();
 
     public GamblingWheelController(int faceCount = 5)
     {
@@ -19,10 +22,17 @@ public class GamblingWheelController : AbstractWheel
         for (var i = 0; i < _symbols.Length; ++i)
             _symbols[i] = ReelIconAdapter.Random(rng);
     }
+
+    public void PrintSymbols()
+    {
+        Debug.Log(string.Join(" ", _symbols.Select(symbol => symbol.ToString())));
+    }
 }
 
 public class GamblingWheel : MonoBehaviour
 {
+    private GamblingController _gamblingController = new GamblingController();
+    private IGamblingBoard _board;
     private RoomController _roomController;
     private GamblingWheelController _wheelController;
 
@@ -31,21 +41,33 @@ public class GamblingWheel : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        _board = _gamblingController.CreateBoard(0);
         _roomController = FindObjectsByType<RoomController>(FindObjectsInactive.Exclude, FindObjectsSortMode.None)
             .First();
         _wheelController = new GamblingWheelController();
         RandomizeSymbols();
+        _board.AddWheel(_wheelController);
     }
 
     void RandomizeSymbols()
     {
         var rng = DeterministicRng.CreateStream(0, "GamblingWheel");
         _wheelController.RandomizeSymbols(rng);
-        Debug.Log("Randomized symbols:");
+        _wheelController.PrintSymbols();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Debug spinning wheel on key press
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            Debug.Log("Space key was pressed!");
+            var steps = _board.GetRandomSteps();
+            _board.PerformSteps(steps);
+            Debug.Log(string.Join(" ", _wheelController.ShownSymbols.Select(symbol => symbol.ToString())));
+        }
     }
+
+
 }
