f7c321f56f0acc9f89d14132461fcc6f6f3a40b9
maxrchung <maxrchung@gmail.com>
2026-01-18 14:17:01 -0800

    cut fingers

--- real-gambling/Assets/_Assets/Scripts/HandScript.cs
+++ real-gambling/Assets/_Assets/Scripts/HandScript.cs
@@ -1,7 +1,11 @@
+using System.Collections;
 using UnityEngine;
 
 public class HandScript : MonoBehaviour
 {
+    public SkinnedMeshRenderer Renderer;
+    public Material Transparent;
+
     private Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -20,12 +24,60 @@ public class HandScript : MonoBehaviour
         animator.SetTrigger("Return");
     }
 
+    public void View()
+    {
+        animator.SetTrigger("View");
+    }
+
+    public void Unview()
+    {
+        animator.SetTrigger("Unview");
+    }
+
+
 
     private int fingers = 5;
 
     public void Cut()
     {
+        StartCoroutine(CutCoroutine());
+    }
 
+    public IEnumerator CutCoroutine()
+    {
+        animator.SetTrigger("View");
+
+        yield return new WaitForSeconds(2);
+
+        var materials = Renderer.materials;
+        if (fingers == 5)
+        {
+            materials[5] = Transparent;
+        }
+        else if (fingers == 4)
+        {
+            materials[4] = Transparent;
+        }
+        else if (fingers == 3)
+        {
+            materials[3] = Transparent;
+        }
+        else if (fingers == 2)
+        {
+            materials[2] = Transparent;
+        }
+        else if (fingers == 1)
+        {
+            // Lose
+            materials[1] = Transparent;
+        }
+
+        Renderer.materials = materials;
+
+        fingers = fingers - 1;
+
+        yield return new WaitForSeconds(1);
+        animator.SetTrigger("Unview");
     }
 
 
